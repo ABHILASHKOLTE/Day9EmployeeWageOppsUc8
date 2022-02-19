615a0b2882b3f2e059a07dc98e8a11b61966ebf9
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2022-02-19 20:33:20 +0530

    AddedUC8 Day 9

--- EmployeeWageUc8.java
+++ EmployeeWageUc8.java
@@ -1,13 +1,14 @@
 package com.day9employeewageUC8;
-
-
-public class EmployeeWageUC8 {
+public class EmployeeWageUc8 {
 
     public static final int IS_PART_TIME = 1, IS_FULL_TIME = 2;
 
     public static void main(String[] args) {
-        computeEmpWage("DMart", 20,2,10);
-        computeEmpWage("DMart", 10,4,20);
+        computeEmpWage("Apple", 30,2,10);
+        computeEmpWage("Sony", 25,4,20);
+        computeEmpWage("Jio", 15,8,20);
+        computeEmpWage("Tesla", 12,6,20);
+        computeEmpWage("Blue Origin", 10,9,20);
     }
 
     public static int computeEmpWage(String company, int EMP_RATE_PER_HOUR, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
@@ -24,10 +25,11 @@ public class EmployeeWageUC8 {
             }
             totalEmpHrs += empHrs;
             dailyWage = empHrs * EMP_RATE_PER_HOUR;
-            System.out.println("Day no.: " + totalWorkingDays + ", Emp Hrs: " +empHrs + ", Daily Wage: " +dailyWage);
+            System.out.println("Day no.: " + totalWorkingDays + ", Emp Hrs: " +empHrs + ", Daily Wage: " +dailyWage );
         }
         int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Employee wage is :" + totalEmpWage);
+        System.out.println("Total Employee wage is :" + totalEmpWage + "\n");
         return totalEmpWage;
     }
 }
+
